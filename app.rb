7a650a4e8e68390aaf61c1cb3e4a54fa9e59191c
olegchursin <olegchursin@gmail.com>
2018-01-09 13:36:16 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+    erb :newteam
+  end
+
+  post '/team' do
+    #binding.pry
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sf = params[:sf]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @c = params[:c]
+    erb :team
+  end
+
 
 end
